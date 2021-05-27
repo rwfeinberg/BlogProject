de8e19d7ec5cd59fe641e731675c884c3c7e7b71
Ryan Feinberg <rwf8@duke.edu>
2021-05-27 17:37:21 -0400

    Re-added key

--- django_project/settings.py
+++ django_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ${{ secrets.SECRET_KEY }}
+SECRET_KEY = 'django-insecure-8kaf)qb0n^e7bid-6khd)%l_=0aa)c6!e-zq-o#4h#q*u(o@ep'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 

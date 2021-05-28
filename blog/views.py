010999a2d03118fdd009fe2e123bb8f9942eba0d
Ryan Feinberg <rwf8@duke.edu>
2021-05-28 17:25:12 -0400

    Created database model, Added database integration to home page

--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'RyanF',
-        'title': 'Post 1',
-        'content': 'This is my first post.',
-        'date_posted': 'May 27, 2021'
-    },
-    {
-        'author': 'SomeoneElse',
-        'title': 'Post 2',
-        'content': 'This is my second post.',
-        'date_posted': 'May 28, 2021'
-    },
-    {
-        'author': 'ThirdPerson',
-        'title': 'Post 3',
-        'content': 'This is my 3rd post.',
-        'date_posted': 'May 29, 2021'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 

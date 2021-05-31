77a03cee7590c37219572417f864296c9025d4d3
Ryan Feinberg <rwf8@duke.edu>
2021-05-31 19:14:59 -0400

    Added post views using classes, routes

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,11 @@
+from blog.models import Post
 from django.urls import path
+from .views import PostListView, PostDetailView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('about/', views.about, name='blog-about'),
     path('special/', views.special, name='special')
 ]

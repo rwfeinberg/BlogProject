e56b5c498d40b55912230bb343b5a07303a2d4eb
Ryan Feinberg <rwf8@duke.edu>
2021-06-07 18:21:08 -0400

    Cleaned up, updated requirements

--- users/models.py
+++ users/models.py
@@ -11,13 +11,13 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'                  # Overriden function to return when profile is called
 
-    def save(self):                                             # Overriden function to correctly update and resize profile pic
-        super().save()
+    # def save(self, *args, **kwargs):                                             # Overriden function to correctly update and resize profile pic
+    #     super().save(*args, **kwargs)
 
-        img = Image.open(self.image.path)
+    #     img = Image.open(self.image.path)
 
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+    #     if img.height > 300 or img.width > 300:
+    #         output_size = (300, 300)
+    #         img.thumbnail(output_size)
+    #         img.save(self.image.path)
 
